35834f0261354398fa464ca4bffc403a7c6f8ed9
danlsgiga <danlsgiga@gmail.com>
2017-02-16 15:49:31 -0700

    Removed instructions from the build script

--- nginx-naxsi/build_nginx.sh
+++ nginx-naxsi/build_nginx.sh
@@ -10,5 +10,3 @@ rpm -ivh http://nginx.org/packages/mainline/centos/7/SRPMS/${NGINX_VERSION}.el7.
 sed -i "s|--with-http_ssl_module|--with-http_ssl_module --with-openssl=/tmp/$OPENSSL_VERSION --add-dynamic-module=/tmp/naxsi-${NAXSI_BRANCH}/naxsi_src|g" /home/builder/rpmbuild/SPECS/nginx.spec && \
 sed -i "s|%files|%files\n%{_libdir}/nginx/modules/ngx_http_naxsi_module.so|g" /home/builder/rpmbuild/SPECS/nginx.spec && \
 rpmbuild -ba /home/builder/rpmbuild/SPECS/nginx.spec
-
-echo 'Use the following command to copy the module binary file: $ docker cp nginx-build:/home/builder/rpmbuild/BUILD/*/objs/ngx_http_naxsi_module.so .'
