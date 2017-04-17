e52acc0f0f19148391c34148a19413bcea0512d2
Daniel Santos <danlsgiga@gmail.com>
2017-04-17 17:59:25 -0600

    Fix more_headers path
    Fix more_headers path on the RPM Spec file

--- nginx/build_nginx.sh
+++ nginx/build_nginx.sh
@@ -11,7 +11,7 @@ tar -xvzf /tmp/more_headers-v${MORE_HEADERS_VERSION}.tar.gz -C /tmp/ && \
 rpm -ivh http://nginx.org/packages/mainline/centos/7/SRPMS/${NGINX_VERSION}.el7.ngx.src.rpm && \
 sed -i "s|--with-http_ssl_module|--with-http_ssl_module --with-openssl=/tmp/$OPENSSL_VERSION|g" /home/builder/rpmbuild/SPECS/nginx.spec && \
 sed -i "s|--with-http_ssl_module|--with-http_ssl_module --add-dynamic-module=/tmp/naxsi-${NAXSI_BRANCH}/naxsi_src|g" /home/builder/rpmbuild/SPECS/nginx.spec && \
-sed -i "s|--with-http_ssl_module|--with-http_ssl_module --add-dynamic-module=/tmp/headers-more-nginx-module-${MORE_HEADERS_VERSION}/src|g" /home/builder/rpmbuild/SPECS/nginx.spec && \
+sed -i "s|--with-http_ssl_module|--with-http_ssl_module --add-dynamic-module=/tmp/headers-more-nginx-module-${MORE_HEADERS_VERSION}|g" /home/builder/rpmbuild/SPECS/nginx.spec && \
 sed -i "s|%files|%files\n%{_libdir}/nginx/modules/ngx_http_naxsi_module.so|g" /home/builder/rpmbuild/SPECS/nginx.spec && \
 sed -i "s|%files|%files\n%{_libdir}/nginx/modules/ngx_http_headers_more_filter_module.so|g" /home/builder/rpmbuild/SPECS/nginx.spec && \
 sed -i "s|%setup -q|%setup -q\nsed -i \"s\|Server: nginx\|Server: ${NGINX_SERVER_HEADER}\|g\" %{bdir}/src/http/ngx_http_header_filter_module.c|g" /home/builder/rpmbuild/SPECS/nginx.spec && \
